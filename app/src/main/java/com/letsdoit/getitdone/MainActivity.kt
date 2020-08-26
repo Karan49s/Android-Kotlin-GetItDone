0ae9646817112ccdffa976192b09157e1db69516
karan49s <karan.4sep@gmail.com>
2020-08-26 22:53:39 +0530

    fragment added

--- app/src/main/java/com/letsdoit/getitdone/MainActivity.kt
+++ app/src/main/java/com/letsdoit/getitdone/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.letsdoit.getitdone
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ft: FragmentTransaction = supportFragmentManager.beginTransaction()
+        ft.replace( R.id.fragholder, TasksFragment())
+        ft.commit()
     }
 }
